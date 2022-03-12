76bdd2eccf61100d1e8dfb01993e325c5f593f8e
lostar01 <2367192101@qq.com>
2022-03-12 09:43:58 +0800

    fix haproxy bug

--- ansible/roles/k8snode/files/kubeadm_install_node.sh
+++ ansible/roles/k8snode/files/kubeadm_install_node.sh
@@ -4,5 +4,5 @@
 install_log=/tmp/kubeadm_install_node.log
 if [ `ls /etc/kubernetes/kubelet.conf &>/dev/null;echo $?` -ne 0 ];then
     kubeadm join 192.168.147.139:16443 --token abcdef.0123456789abcdef \
-	--discovery-token-ca-cert-hash sha256:fbb7630ff1cd017e485cad0869fe0f28d4eef447f30449fa1a08732752ec4f81   &> $install_log
+	--discovery-token-ca-cert-hash sha256:b72731bd3a76a3e741c7a7d4c795bb36a18b28e615e7297bd90c2418c0a9cfe7   &> $install_log
 fi
